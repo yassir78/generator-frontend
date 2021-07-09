27730c825eefa41be1f1932e01c5f36bf09048b4
AZMI OUMAYMA <oumaymaazmi@gmail.com>
2021-07-09 16:47:49 +0100

    oumayma export yaml initialized

--- src/app/view/admin/pojos/pojo-list/pojo-list.component.ts
+++ src/app/view/admin/pojos/pojo-list/pojo-list.component.ts
@@ -4,6 +4,9 @@ import {PojoService} from '../../../../controller/service/pojo.service';
 import {Pojo} from '../../../../controller/model/pojo';
 import {UserConfig} from "../../../../controller/model/userConfig";
 import {Router} from "@angular/router";
+var YAML = require('json2yaml');
+import { saveAs } from 'file-saver';
+import {Field} from "../../../../controller/model/field";
 
 
 @Component({
@@ -13,7 +16,7 @@ import {Router} from "@angular/router";
   providers: [MessageService, ConfirmationService]
 })
 export class PojoListComponent implements OnInit {
-  appear:boolean=false;
+  appear : boolean = false;
   cols: any[];
   colAttributs:any[];
   constructor(private messageService: MessageService, private confirmationService: ConfirmationService,
@@ -67,9 +70,37 @@ export class PojoListComponent implements OnInit {
     });
   }
 
-  public details(selected: Pojo){
-    this.appear=true;
-    this.selected = selected;
+ public prepareItems(items: Pojo[]){
+    const results: {} = {};
+    items.forEach(item =>{
+      const fields = {};
+      item.fields.forEach(field => {
+        fields[`${field.name}`] = `${field.type.simpleName}`;
+        //fields.push(this.prepareField(field));
+      })
+      results[`${item.name}`] = fields;
+    })
+
+   return results;
+
+ }
+
+ public prepareField(field: Field){
+    let result: {} = {};
+    result[`${field.name}`] = field.type.simpleName;
+    return result;
+ }
+  public onExport(){
+    const items = this.prepareItems(this.items)
+    //console.log(items);
+    const ymlText = YAML.stringify(items);
+     console.log(ymlText);
+     const string : String = new String(ymlText)
+    ymlText.replace("/-/g","")
+    console.log("ymlText");
+    console.log(string);
+    var myFile = new File([ymlText], "demo.yaml", {type: "text/yaml;charset=utf-8"});
+    saveAs(myFile);
   }
 
 
