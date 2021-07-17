aec3006a223df1a97c0b0146999c6afebabd7a5e
AZMI OUMAYMA <oumaymaazmi@gmail.com>
2021-07-17 12:32:57 +0100

    oum,l

--- src/app/view/admin/pojos/pojo-list/pojo-list.component.ts
+++ src/app/view/admin/pojos/pojo-list/pojo-list.component.ts
@@ -13,7 +13,7 @@ import { Pojo } from "../../../../controller/model/pojo";
 import { Router } from "@angular/router";
 import {Field} from "../../../../controller/model/field";
 import { saveAs } from 'file-saver';
-var YAML = require("json2yaml");
+import * as YAML from "json2yaml";
 @Component({
   selector: "app-pojo-list",
   templateUrl: "./pojo-list.component.html",
